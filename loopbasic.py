38aa5309eafa26054bc66261b3026106ab314f5a
Akirakone <ca646@khels.org>
2021-11-01 22:39:58 -0400

    loop

---
+++ loopbasic.py
@@ -0,0 +1,38 @@
+
+for i in range(0,151):
+    print (i)
+
+for i in range(5,1001):
+    if i%5==0:
+       print (i)
+
+for i in range(1,101):
+    if i%10==0:
+        print("codingdojo")
+        
+    elif i%5==0:
+        print("coding")
+
+    else:
+        print (i)
+
+sum=0
+for i in range (0,500001):
+    if i %3==0:
+        sum==i
+    print (sum)
+    
+lownum=2
+highnum=9
+multi=3
+for i in range (lownum, highnum):
+        if i %multi==0 and i!=0:
+            print(i)
+
+for i in range(2018,0,-4):
+    print (i)
+
+
+
+
+
